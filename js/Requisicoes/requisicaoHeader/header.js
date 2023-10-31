f4a79d3fd72fbf471066d2ad50ce2c248413a239
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-10-30 21:28:09 -0300

    Telas de Carrinho

--- js/Requisicoes/requisicaoHeader/header.js
+++ js/Requisicoes/requisicaoHeader/header.js
@@ -1,5 +1,5 @@
 async function loadHeader() {
-    const resposta = await fetch('https://myndofgangster-chi.vercel.app/components/header.html')
+    const resposta = await fetch('http://localhost/Sites/Repositorios/Loja-2.0/components/header.html')
     const headerText = await resposta.text()
     const headerContainer = document.querySelector('.header');
     console.log(headerText)
