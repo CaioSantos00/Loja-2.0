c324e5f86b4083bbce8cf311464d496fe0a73b56
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-12-15 23:19:36 -0300

    Up

--- js/Requisicoes/requisicaoHeader/header.js
+++ js/Requisicoes/requisicaoHeader/header.js
@@ -9,12 +9,14 @@ async function loadHeader() {
 let removeMenu = document.getElementById('removeMenu')
 let nav = document.querySelector('nav')
 addMenu.addEventListener('click', ()=> {
-	nav.classList.add('navMenu')
+    nav.style.transform = 'translateY(0)'
+	//nav.classList.add('navMenu')
 	console.log(nav)
 })
 
 removeMenu.addEventListener('click', ()=> {
-	nav.classList.remove('navMenu')
+	//nav.classList.remove('navMenu')
+    nav.style.transform = 'translateY(-100%)';
 })
 }
 
