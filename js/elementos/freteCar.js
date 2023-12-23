07a164a9ee5a9e2376674b9e9e7ad66204705897
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-12-22 21:52:19 -0300

    Atualização

---
+++ js/elementos/freteCar.js
@@ -0,0 +1,16 @@
+let divFrete = document.getElementById('divFrete'),
+    divInputFrete = document.getElementById('divInputFrete')
+let descriptionFalseTrue = false
+
+divFrete.addEventListener('click', () =>{
+    descriptionFalseTrue = !descriptionFalseTrue
+
+    divFrete.style.overflow = 'visible'
+    divFrete.style.height = '100%'
+    divInputFrete.style.transform = 'translateY(0px)'
+    if (!descriptionFalseTrue) {
+        divFrete.style.overflow = 'hidden'
+        divInputFrete.style.transform = 'translateY(100%)'
+        divFrete.style.height = '20px'
+    }
+})
